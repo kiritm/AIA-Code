a01b485f7ec3ec42f1d373909e064cb0a4594c27
Kirit Maganlal Shah <kiritshah2@yahoo.com.sg>
2021-10-05 13:14:48 +0800

    updating data for page routing

--- sampleApp/src/app/login/login.component.ts
+++ sampleApp/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
+
+import { HttpClient } from '@angular/common/http';
+//import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public loginForm !: FormGroup;
+  constructor(private FormBuilder : FormBuilder,  private http : HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.FormBuilder.group({
+      email:[''],
+      password:['']
+    })
   }
-
+  login(){
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        alert("found user :" + a.email );
+        alert(" search email from form"+this.loginForm.value.email);
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if(user){
+        alert("Login success.");
+        this.loginForm.reset();
+        this.router.navigate(['dashboard'])
+      }else{
+        alert("User not found");
+      }     
+    },err=>{
+      alert("User registration fail got error.")    
+    })
+  }
 }
